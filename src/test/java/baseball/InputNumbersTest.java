cf7816d160b7d0e7a9be88ac41fd9078745b2c0b
limwoobin <drogba02@naver.com>
2022-04-13 23:28:39 +0900

    <test>: 사용자가 입력한 값에 대한 통합 Validator 테스트 코드를 작성한다
    - 입력한 값이 정상적이면 모든 validator 를 통과하고 List Collection 을 리턴한다
    - 입력한 값이 문제가 있다면 validator 에 걸려 IllegalArgumentException 을 리턴한다

--- src/test/java/baseball/InputNumbersTest.java
+++ src/test/java/baseball/InputNumbersTest.java
@@ -1,15 +1,25 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import baseball.domain.input.InputNumbers;
 import baseball.domain.input.validator.InputNumbersSpaceValidator;
 import baseball.domain.input.validator.InputNumbersNumberFormatValidator;
 import baseball.domain.input.validator.InputNumbersOverlapValidator;
 import baseball.domain.input.validator.InputNumbersSizeValidator;
 import baseball.domain.input.validator.InputNumbersValidator;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
 public class InputNumbersTest {
 
@@ -59,6 +69,66 @@ public class InputNumbersTest {
             assertThatThrownBy(() -> {
                 validator.execute(input);
             }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+    @Nested
+    @DisplayName("입력값이 정상적이라면 정상적인 List<Integer> 의 형태로 리턴되어야 한다")
+    class UserInputSuccessTest {
+        private MockedStatic<Console> console;
+
+        @BeforeEach
+        void before() {
+            console = mockStatic(Console.class);
+        }
+
+        @AfterEach
+        void after() {
+            console.close();
+        }
+
+        @DisplayName("입력값이 정상적인 값이라면 List 형태로 리턴되어야 한다")
+        @ParameterizedTest
+        @ValueSource(strings = {"123" , "134" , "145" , "456"})
+        void input_success_test(String input) {
+            InputNumbers inputNumbers = new InputNumbers();
+
+            when(Console.readLine())
+                    .thenReturn(input);
+
+            List<Integer> result = inputNumbers.createInputNumbers();
+            String[] inputValues = input.split("");
+
+            assertThat(result.get(0)).isEqualTo(Integer.parseInt(inputValues[0]));
+            assertThat(result.get(1)).isEqualTo(Integer.parseInt(inputValues[1]));
+            assertThat(result.get(2)).isEqualTo(Integer.parseInt(inputValues[2]));
+        }
+    }
+
+    @Nested
+    @DisplayName("입력값이 올바르지 않다면 IllegalArgumentException 을 발생시켜야 한다")
+    class UserInputFailedTest {
+        private MockedStatic<Console> console;
+
+        @BeforeEach
+        void before() {
+            console = mockStatic(Console.class);
+        }
+
+        @AfterEach
+        void after() {
+            console.close();
+        }
+
+        @DisplayName("입력값이 올바르지 않다면 IllegalArgumentException 을 발생시켜야 한다")
+        @ParameterizedTest
+        @ValueSource(strings = {"1 34" , "112" , "12343" , "qwdewe" , "1w3"})
+        void input_failed_test(String input) {
+            InputNumbers inputNumbers = new InputNumbers();
+
+            when(Console.readLine())
+                    .thenReturn(input);
+
+            assertThatThrownBy(inputNumbers::createInputNumbers).isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
